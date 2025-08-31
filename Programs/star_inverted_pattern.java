93efc013b83b55c4efd8f0f0a62f5b9d42535e3c
Manojshekar <manojshekarc63@gmail.com>
2025-08-31 22:32:33 +0530

    Initial commit

---
+++ Programs/star_inverted_pattern.java
@@ -0,0 +1,16 @@
+public class star_inverted_pattern {
+    public static void main(String args[]) {
+
+            int n= 4 ;
+         for (int line = 0; line <4; line++) {
+            for (int star = 1; star<=4-line; star++) {
+               System.out.print("*"); 
+            }
+            System.out.print("\n");
+
+            
+         }
+
+    }
+    
+}
